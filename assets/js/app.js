336d2a290a17b7a1aa919f035fd633187b103f0f
john <john@DESKTOP-BMPNAJC>
2017-02-16 23:51:00 -0800

    new repostory easyway inventory

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,27 @@
 var myApp=angular.module('myApp',["ngRoute"]);
 myApp.config(function($routeProvider){
     $routeProvider
-            .when("/",{   
+            .when("/",{
+                resolve:{
+                        "check":function($location){
+                          var logged=sessionStorage.getItem('logged'); 
+                          if(logged=="true"){
+                              $location.path("/dashboard");
+                          }
+                        }
+                    },
                    controller:'loginController',
                    templateUrl:'app/src/views/login.html' 
                 })
             .when("/dashboard",{
+                    resolve:{
+                        'check':function($location){
+                          var logged=sessionStorage.getItem('logged'); 
+                          if(!logged){
+                              $location.path("/");
+                          }
+                        }
+                    },
                     controller:'dashboardController',
                     templateUrl:'app/src/views/dashboard.html'
                 })
--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,16 +1,9 @@
 myApp.controller("loginController",function($scope,$location,loginService){       
-        var user=$scope.user;
-        var pass=$scope.pass;       
+               
     //get login  onclick
-    $scope.login=function(){            
-        if($scope.user=="" || $scope.user=="undefined"){
-            $scope.errMsg="Plese enter user Id";
-            return false;
-        }
-        if($scope.pass=="" || $scope.pass=="undefined"){
-            $scope.errMsg="Plese enter Password";
-            return false;
-        }
+    $scope.login=function(){
+        var user=$scope.user;
+        var pass=$scope.pass;
         var resultObj=loginService.login(user,pass);        
         if(resultObj.result=="success"){
             sessionStorage.setItem("token",resultObj.ACCESS_TOKEN); 
@@ -26,9 +19,16 @@ myApp.controller("loginController",function($scope,$location,loginService){
 });
 
 
-myApp.controller("dashboardController",function($scope){
+myApp.controller("dashboardController",function($scope,$location){
     var admin=sessionStorage.getItem("admin");
         adminObj=JSON.parse(admin);
    $scope.userId=adminObj.USER_ID;
+   
+   //logout funciton 
+    $scope.logout=function(){
+       sessionStorage.setItem("logged","");
+       $location.path("/");
+   }
+    //end
     
 })
